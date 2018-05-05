2a4c832e6aaf37e56028744a2515c57a683ef068
Will Carter <willmckenna64@gmail.com>
2018-05-05 06:21:53 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,4 +1,8 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
+<<<<<<< HEAD
 print "   |   |   \n-----------\n   |   |   \n-----------\n   |   |   \n"
+=======
+print "   |   |   "\n"   |   |   "\n"   |   |   "
+>>>>>>> 849a358fdbda92a8250c35c3c6f217a8690e5eb6
 end
